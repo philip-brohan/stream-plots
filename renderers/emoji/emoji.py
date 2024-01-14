b40fb913a1340f1900a9516285ff8640f539b774
Philip Brohan <philip@brohan.org>
2024-01-14 22:47:39 +0000

    Added and emoji renderer.

---
+++ renderers/emoji/emoji.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+from PIL import Image, ImageDraw, ImageFont
+from fontTools.ttLib import TTFont
+import random
+
+# Emoji font to use
+pad = 10  # Padding between emoji
+fontsize = 109  # Fixed
+font = ImageFont.truetype(
+    r"/Users/philip/miniconda3/envs/stp/fonts/NotoColorEmoji.ttf", fontsize
+)
+# Get a list of all the code points in the font
+font2 = TTFont(r"/Users/philip/miniconda3/envs/stp/fonts/NotoColorEmoji.ttf")
+# Get the 'cmap' table, which maps character codes to glyph names
+cmap = font2.getBestCmap()
+# The keys of the 'cmap' table are the supported Unicode code points
+supported_code_points = list(cmap.keys())
+
+# Image to draw on
+plot_width = 50  # characters
+plot_height = 50
+bgcol = (225, 225, 225)
+img = Image.new(
+    mode="RGB",
+    size=(plot_width * (fontsize + pad) + pad, plot_height * (fontsize + pad) + pad),
+    color=bgcol,
+)
+draw = ImageDraw.Draw(img)
+
+# Draw random emoji
+for h in range(plot_height):
+    for w in range(plot_width):
+        pChar = chr(random.choice(supported_code_points))
+        draw.text(
+            (w * (fontsize + pad) + pad / 2, h * (fontsize + pad) + pad / 2),
+            pChar,
+            font=font,
+            embedded_color=True,
+        )
+
+img.save("emoji.webp", "webp", lossless=True)
